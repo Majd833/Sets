73381e8fc69fd8bff9c3e1d07cecc9d795d127cc
Majd833 <lina.azzam.1611@gmail.com>
2024-11-14 17:56:50 +0200

    Set Intersection

---
+++ Sets Intersection.py
@@ -0,0 +1,8 @@
+setx={"White", "Black"}
+sety={"Black", "Blue"}
+print("The original elements are:")
+print(setx)
+print(sety)
+print("The common between the two sets is:")
+setz=setx.intersection(sety)
+print(setz)
