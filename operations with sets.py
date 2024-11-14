85e1a83680bf63ce8fbeadefccd32a9d25fbecf1
Majd833 <lina.azzam.1611@gmail.com>
2024-11-14 17:52:09 +0200

    Operatiions with sets

---
+++ operations with sets.py
@@ -0,0 +1,10 @@
+sets={1,2,4,5}
+print(sets)
+setss={1.3,"hello",8}
+print(setss)
+my_set=set([1,3,5,3,2,8,2])
+print(my_set)
+poping_set={0,1,2,3,5,6}
+print("This is the original set:",poping_set)
+poping_set.pop()
+print(poping_set)
